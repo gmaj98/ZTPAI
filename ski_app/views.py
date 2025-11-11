25990e17282ec6516988a0e3da8c8c7cad6b2d53
gmaj98 <122926970+gmaj98@users.noreply.github.com>
2025-11-11 16:03:38 +0100

    Delete ski_app directory

--- ski_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import Http404, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
-from django.urls import reverse
-from django.template.loader import render_to_string
-from django.http import JsonResponse
-
-
-
