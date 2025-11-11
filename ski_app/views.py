63330294eb52ebf0151cb65f6749b5f94c3aadbb
gmaj98 <122926970+gmaj98@users.noreply.github.com>
2025-11-11 15:56:52 +0100

    Add files via upload

---
+++ ski_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+
+
+
